c99a314ccb467383efda067dda8a3e3b9fe42326
Fevers <47673608+FortniteFevers@users.noreply.github.com>
2022-01-13 20:52:39 -0500

    fixed loadout shit

--- bot.py
+++ bot.py
@@ -9,13 +9,13 @@ from discord.ext.commands import bot
 import json
 import requests
 
-testing_guilds = [926178688247140383]
+testing_guilds = []
 
 client = commands.Bot(command_prefix='............', intents=intents)
 client.remove_command('help')
 slash = SlashCommand(client, sync_commands = True)
 
-headerslmao = {'Authorization': '60bcad5e-fe7c-4734-92a9-986b81f99444'}
+headerslmao = {'Authorization': AUTH_TOKEN}
 
 @client.event
 async def on_ready():
@@ -36,7 +36,7 @@ async def login(ctx, auth:str = None):
         embed = discord.Embed(
             color = discord.Colour.blue(),
             title='Login to your Epic Games account',
-            description='[CLICK ME TO GET YOUR AUTH CODE](https://www.epicgames.com/id/api/redirect?clientId=3446cd72694c4a4485d81b77adbb2141&responseType=code)\n\nHow to login to your Epic Games account:\n\n1. Visit the link above to get your login code.\n2. Copy the 32 character code that looks like **aabbccddeeff11223344556677889900**, located after **authorizationCode=**.\n3. Send /login <32 character code> to complete your login.'
+            description='[CLICK ME TO GET YOUR AUTH CODE](https://www.epicgames.com/id/login?redirectUrl=https%3A%2F%2Fwww.epicgames.com%2Fid%2Fapi%2Fredirect%3FclientId%3Dec684b8c687f479fadea3cb2ad83f5c6%26responseType%3Dcode)\n\nHow to login to your Epic Games account:\n\n1. Visit the link above to get your login code.\n2. Copy the 32 character code that looks like **aabbccddeeff11223344556677889900**, located after **authorizationCode=**.\n3. Send /login <32 character code> to complete your login.\n\n**Need to switch accounts?**\n[Use this link instead](https://www.epicgames.com/id/login?redirectUrl=https%3A%2F%2Fwww.epicgames.com%2Fid%2Fapi%2Fredirect%3FclientId%3D3f69e56c7649492c8cc29f1af08a8a12%26responseType%3Dcode&prompt=login)'
         )
         embed.set_footer(text='We recommend that you only log into accounts that you have email access to!')
 
@@ -44,7 +44,7 @@ async def login(ctx, auth:str = None):
     else:
         response = requests.post('https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token', headers={
             "Content-Type": "application/x-www-form-urlencoded",
-            "Authorization": f"basic MzQ0NmNkNzI2OTRjNGE0NDg1ZDgxYjc3YWRiYjIxNDE6OTIwOWQ0YTVlMjVhNDU3ZmI5YjA3NDg5ZDMxM2I0MWE="
+            "Authorization": f"basic ZWM2ODRiOGM2ODdmNDc5ZmFkZWEzY2IyYWQ4M2Y1YzY6ZTFmMzFjMjExZjI4NDEzMTg2MjYyZDM3YTEzZmM4NGQ="
         },
         data={
             "grant_type": "authorization_code",
@@ -63,7 +63,7 @@ async def login(ctx, auth:str = None):
         accountID = response.json()['account_id']
 
         #await ctx.send(f"Generated auth token. Expires at {response.json()['expires_at']}")
-        await ctx.author.send(f"Auth Token: {token}\nExpires at {response.json()['expires_at']}")
+        await ctx.author.send(f"I have just generated a new token for you.\nThis token Expires at {response.json()['expires_at']}")
 
         a_file = open(f"auths.json", "r")
         json_object = json.load(a_file)
@@ -86,6 +86,8 @@ async def login(ctx, auth:str = None):
                 "accountID": f"{accountID}",
                 "loadoutUUID": ""
             })
+            a_file = open(f"auths.json", "w")
+            json.dump(json_object, a_file, indent = 4)
         else:
             json_object['token'] = str(token)
             for i in json_object['auths']:
@@ -94,7 +96,10 @@ async def login(ctx, auth:str = None):
                     i['token'] = token
 
         response = requests.get(f'https://fortnite-api.com/v2/stats/br/v2/{accountID}', headers=headerslmao)
-        clientUsername = response.json()['data']['account']['name']
+        try:
+            clientUsername = response.json()['data']['account']['name']
+        except:
+            clientUsername = 'error'
 
         response = requests.post(f'https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/profile/{accountID}/client/QueryProfile?profileId=athena',  json={"text": {}}, headers={
             "Authorization": f"Bearer {token}",
@@ -104,7 +109,9 @@ async def login(ctx, auth:str = None):
         
         #print(response.json())
         created = response.json()['profileChanges'][0]['profile']['created']
+        created = created[:10]
         updated = response.json()['profileChanges'][0]['profile']['updated']
+        updated = updated[:10]
 
         embed = discord.Embed(
             color = discord.Colour.red(),
@@ -139,7 +146,7 @@ def authenticate():
     json_object = json.load(a_file)
     a_file.close()
 
-@slash.slash(name='showlocker', description='Shows the skins in your Fortnite locker', guild_ids=testing_guilds)
+@slash.slash(name='loadout', description='Shows your current Fortnite loadout', guild_ids=testing_guilds)
 async def showlocker(ctx):
     a_file = open(f"auths.json", "r")
     json_object = json.load(a_file)
@@ -173,48 +180,218 @@ async def showlocker(ctx):
                 
                 clientUsername = response.json()['data']['account']['name']
                 embed = discord.Embed(
-                    color = discord.Colour.blue(),
-                    title=f"{clientUsername}'s locker"
+                    color = discord.Colour.green(),
+                    title=f"{clientUsername}'s current loadout"
                 )
-                await ctx.send('Sucesfully got into the API!')
+                message = await ctx.send('Loading locker...')
 
-                lockerskinID = lockerdata['Character']['items'][0]
-                lockerskinID = lockerskinID.replace('AthenaCharacter:', '')
-                response = requests.get(f'https://fortnite-api.com/v2/cosmetics/br/search?id={lockerskinID}')
-                skinname = response.json()['data']['name']
+                list_ = []
+                for i in lockerdata:
+                    #print(i)
+                    backendtype = i
+                    type = lockerdata[i]
+                    list_.append({
+                        f'{i}': [],
+                        "backendType": backendtype
+                    })
+                    for i in type['items']:
+                        
+                        id = i.split(":").pop()
+                        if i == '':
+                            id = 'None'
+                        
+                        for i in list_:
+                            if backendtype in i:
+                                i[backendtype].append(id)
 
-                lockerbackpackID = lockerdata['Backpack']['items'][0]
-                lockerbackpackID = lockerbackpackID.replace('AthenaBackpack:', '')
-                if lockerbackpackID != '':
-                    response = requests.get(f'https://fortnite-api.com/v2/cosmetics/br/search?id={lockerskinID}')
-                    backpack = response.json()['data']['name']
-                else:
-                    backpack = 'Empty'
 
-                emotes = ''
-                for i in lockerdata['Dance']['items']:
-                    emoteID = i.replace('AthenaDance:', '')
-                    response = requests.get(f'https://fortnite-api.com/v2/cosmetics/br/search?id={emoteID}')
-                    name = response.json()['data']['name']
-                    emotes += f'{name}\n'
+                for i in list_:
+                    backendtype = i['backendType']
+                    cosmetics = ''
+                    for i in i[backendtype]:
+                        if i != 'None':
+                            #print(i)
+                            response = requests.get(f'https://fortnite-api.com/v2/cosmetics/br/search?id={i}')
+                            i = response.json()['data']['name']
+                            url = response.json()['data']['images']['icon']
+                        cosmetics += f'[{i}]({url})\n'
+                    if i != 'None':
+                        embed.add_field(
+                            name=backendtype,
+                            value=cosmetics
+                        )
+                    else:
+                        embed.add_field(
+                            name=backendtype,
+                            value='N/A'
+                        )
+                    
 
-
-                
-                embed.add_field(
-                    name='Current Skin',
-                    value=f'{skinname}'
-                )
-                
-                embed.add_field(
-                    name='Current Backpack',
-                    value=f'{backpack}'
-                )   
-
-                embed.add_field(
-                    name='Current Emotes',
-                    value=f'{emotes}'
-                )                                       
-
+                await message.delete()
                 await ctx.send(embed=embed)
 
-client.run('')
+
+@slash.slash(name='purchaceitem', description='Purchace an item from the current shop!',options=[
+    create_option(
+        name='offerid',
+        description='Enter item Offer ID (Must include v2:/)',
+        option_type=3,
+        required=True
+    )
+], guild_ids=testing_guilds)
+async def purchaceitem(ctx, offerid:str):
+    print('hello')
+    a_file = open(f"auths.json", "r")
+    json_object = json.load(a_file)
+    a_file.close()
+
+    DiscordauthorID = ctx.author.id
+
+    for i in json_object['auths']:
+        if i['DiscordauthorID'] == str(DiscordauthorID):
+            #await ctx.send('Loaded auth token!')
+            token = i['token']
+            accountID = i['accountID']
+            response = requests.post(f'https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/profile/{accountID}/client/PurchaseCatalogEntry?profileId=common_core',  json={"text": {
+                "offerId": "v2:/fcd259b7616f9e189a4d9dc5a2e75c20370823b1b9983f034e46738662ee18bd",
+                "purchaseQuantity": 1,
+                "currency": "MtxCurrency",
+                "expectedTotalPrice": 0,
+                "gameContext": "",
+                "currencySubType": ""
+            }}, headers={
+                "Authorization": f"Bearer {token}",
+                "Content-Type": "application/json"
+            }
+            )
+
+            print(response.json())
+            try:
+                error = response.json()['errorMessage']
+                embed = discord.Embed(
+                    color = discord.Colour.red(),
+                    title='ERROR',
+                    description=error
+                )
+                return await ctx.send(embed=embed)
+            except:
+                await ctx.send('Purchased item!')
+
+@slash.slash(name='sac', description='Change your support-a-creator code!',options=[
+    create_option(
+        name='code',
+        description='Support-A-Creator Code',
+        option_type=3,
+        required=True
+    )
+], guild_ids=testing_guilds)
+async def purchaceitem(ctx, code:str):
+    a_file = open(f"auths.json", "r")
+    json_object = json.load(a_file)
+    a_file.close()
+
+    DiscordauthorID = ctx.author.id
+
+    for i in json_object['auths']:
+        if i['DiscordauthorID'] == str(DiscordauthorID):
+            #await ctx.send('Loaded auth token!')
+            token = i['token']
+            accountID = i['accountID']
+            response = requests.post(f'https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/profile/{accountID}/client/SetAffiliateName?profileId=common_core',  json={"text": {
+                "affiliateName": f"{code}"
+            }}, headers={
+                "Authorization": f"Bearer {token}",
+                "Content-Type": "application/json"
+            }
+            )
+
+            print(response.json())
+            try:
+                error = response.json()['errorMessage']
+                embed = discord.Embed(
+                    color = discord.Colour.red(),
+                    title='ERROR',
+                    description='Your token has most likely expired! Type /login <auth> to generate a new one.'
+                )
+                return await ctx.send(embed=embed)
+            except:
+
+                await ctx.send(f'Successfully changed Support-A-Creator Code to "{code}"!')
+
+
+@slash.slash(name='gift', description='Gift an item thats currently in the shop!',options=[
+    create_option(
+        name='offerid',
+        description='Item OfferID',
+        option_type=3,
+        required=True
+    ),
+    create_option(
+        name='price',
+        description='The price of the item (MUST ENTER REAL PRICE OR IT WILL NOT WORK)',
+        option_type=3,
+        required=True
+    ),
+    create_option(
+        name='user',
+        description='The username of the account you want to gift to',
+        option_type=3,
+        required=True
+    )
+], guild_ids=testing_guilds)
+async def gift(ctx, offerid:str, user:str, price:int):
+    a_file = open(f"auths.json", "r")
+    json_object = json.load(a_file)
+    a_file.close()
+
+    DiscordauthorID = ctx.author.id
+    response = requests.get(f'https://fortnite-api.com/v2/stats/br/v2?name={user}', headers=headerslmao)
+    user_name = response.json()['data']['account']['name']
+    user_id = response.json()['data']['account']['id']
+
+    if response.json()['status'] != 200:
+        error = response.json()['error']
+        embed = discord.Embed(
+            color = discord.Colour.red(),
+            title='ERROR',
+            description=f'{error}'
+        )
+        return await ctx.send(embed=embed)
+    
+
+    for i in json_object['auths']:
+        if i['DiscordauthorID'] == str(DiscordauthorID):
+            #await ctx.send('Loaded auth token!')
+            token = i['token']
+            accountID = i['accountID']
+            response = requests.post(f'https://fortnite-public-service-prod11.ol.epicgames.com/fortnite/api/game/v2/profile/{accountID}/client/GiftCatalogEntry?profileId=common_core',  json={"text": {
+                "offerId": f"{offerid}",
+                "purchaseQuantity": 1,
+                "currency": "MtxCurrency",
+                "expectedTotalPrice": price,
+                "gameContext": "",
+                "receiverAccountIds": [f"{user_id}"],
+                "giftWrapTemplateId": "GiftBox:gb_default",
+                "currencySubType": ""
+            }}, headers={
+                "Authorization": f"Bearer {token}",
+                "Content-Type": "application/json"
+            }
+            )
+
+            print(response.json())
+            try:
+                error = response.json()['errorMessage']
+                embed = discord.Embed(
+                    color = discord.Colour.red(),
+                    title='ERROR',
+                    description='Your token has most likely expired! Type /login <auth> to generate a new one.'
+                )
+                return await ctx.send(embed=embed)
+            except:
+
+                await ctx.send(f'Successfully gifted item to "{user_name}"!')
+
+
+
+client.run(TOKEN)
